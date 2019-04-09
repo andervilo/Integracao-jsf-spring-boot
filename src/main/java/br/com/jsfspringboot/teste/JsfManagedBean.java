4c9ee72431d5c00282ceb2b9943e3f68f1791741
andervilo <andervilo@hotmail.com>
2019-04-09 17:00:40 -0300

    .

---
+++ src/main/java/br/com/jsfspringboot/teste/JsfManagedBean.java
@@ -0,0 +1,26 @@
+package br.com.jsfspringboot.teste;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ManagedBean
+public class JsfManagedBean {
+
+    private String titulo = "O managed bean desta página está anotado com @ManagedBean, ou seja, podemos ter beans JSF e Spring em um mesmo projeto!";
+
+    @Autowired
+    private ClienteService clienteService;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ClienteService getClienteService() {
+        return clienteService;
+    }
+}
---
+++ src/main/java/br/com/jsfspringboot/teste/PesquisaClienteBean.java
@@ -0,0 +1,29 @@
+package br.com.jsfspringboot.teste;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("view")
+public class PesquisaClienteBean {
+
+    private Collection<Cliente> clientes = Collections.emptyList();
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostConstruct
+    public void postConstruct() {
+        clientes = clienteService.pesquisar();
+    }
+
+    public Collection<Cliente> getClientes() {
+        return clientes;
+    }
+}
