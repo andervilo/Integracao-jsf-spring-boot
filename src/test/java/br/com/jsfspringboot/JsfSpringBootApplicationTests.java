b4f019b03233ea0643cef03819ac80ae62a5f9ed
andervilo <andervilo@hotmail.com>
2019-05-10 17:18:41 -0300

    .

--- src/test/java/br/com/jsfspringboot/JsfSpringBootApplicationTests.java
+++ src/test/java/br/com/jsfspringboot/JsfSpringBootApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import br.com.jsfspringboot.repository.ColaboradorRepository;
 
 @RunWith(SpringRunner.class)
